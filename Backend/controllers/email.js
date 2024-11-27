a66d1152e462a3d5d4c16ff3693b27c74b0b3bd5
Ojas Dudhabaware <ojasdudhabaware@gmail.com>
2024-11-27 17:20:14 +0530

    Major Project

---
+++ Backend/controllers/email.js
@@ -0,0 +1,54 @@
+const nodemailer = require('nodemailer');
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
+
+
+const transporter = nodemailer.createTransport({
+  service: 'Gmail',
+  auth: {
+    user: process.env.GMAIL_USER,
+    pass: process.env.GMAIL_PASS,
+  },
+});
+
+
+
+async function sendMail( type,id, email) {
+
+  if (type=="verify") {
+    // console.log(process.env.GMAIL_USER);
+
+    try {
+      const emailToken = jwt.sign({ id: id }, "email1234", {
+        expiresIn: '1d',
+      })
+      const url = `https://germanclasses.onrender.com/confirmation/${emailToken}`;
+      // const showUrl = `http://localhost:3000/confirm/email`;
+
+      await transporter.sendMail({
+        to: email,
+        subject: 'Confirm Email',
+        html: `Please click this email to confirm your email: <a href="${url}">${url}</a>`,
+      });
+      return true;
+    } catch (e) {
+      return false;
+    }
+  }
+  else {
+    try {
+     
+      await transporter.sendMail({
+        to: email,
+        subject: 'Payment Done',
+        html: `Your payment has been done successfully. Your payment id is : ${id} for any payment related query please mail us at support@class.com with your payment id`,
+      });
+      return true;
+    } catch (e) {
+      return false;
+    }
+  }
+}
+
+module.exports = sendMail;
